3ff324aa18f518f831b4b7628d30c3b454d72380
Jack <jbroncato@luc.edu>
2019-02-22 16:23:27 -0600

    desktop merge

--- server.js
+++ server.js
@@ -11,51 +11,8 @@ mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true}).cat
 });
 // A map of functions which return data for the schema.
 const jesolvers = require('./src/resolvers');
-<<<<<<< HEAD
-console.log(jesolvers);
-const resolvers = {
-  Query: {
-  findUser: async(parent,args) => {
-    let user = {
-      email: 'test'
-    }
-    console.log(user.email)
-    return user
-  }
-}
-}
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-  email: String,
-  password: String
-});
-
-var Usermodel = mongoose.model('Usermodel', userSchema);
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// The GraphQL schema
-const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    currUser(ida: ID!): User,
-    findUser(email: String!): User
-  }, type User {
-    "user info"
-    id: ID
-    email: String
-    password: String
-  },
-  type Mutation {
-    signUp(email: String!,password: String!): User!
-  }
-`;
-
-=======
 
 app.get('/', (req, res) => res.send('Hello World!'));
->>>>>>> 9cf377569f324dc0c028b9b353ce827454c44979
  
 const server = new ApolloServer({
   // These will be defined for both new or existing servers
